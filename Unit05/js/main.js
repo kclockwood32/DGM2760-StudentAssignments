341e5c3b6163855e007ef4bf533ff73323a3b3f1
Casey Lockwood <kclockwood32@gmail.com>
2022-10-06 13:46:55 -0600

    Added structure for assgnmt 5

---
+++ Unit05/js/main.js
@@ -0,0 +1,6 @@
+// Create an array with 4 trees listed
+const trees = ['oak', 'Pine', 'aspen', 'Bald Cypress']
+const errorElement = document.querySelector('#error')
+const displayResults = document.querySelector('#displayResults')
+
+displayResults.textContent = 'Testing here'
