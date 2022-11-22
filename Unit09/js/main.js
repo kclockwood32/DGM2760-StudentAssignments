f225b1033ca1c6553b543ae4476c8669961e1079
Casey Lockwood <kclockwood32@gmail.com>
2022-11-21 18:49:45 -0700

    Finished assignment 9

--- Unit09/js/main.js
+++ Unit09/js/main.js
@@ -1,6 +1,7 @@
 function duplicateMenu() {
     // get all of the anchor elements from the top menu
     let topList = document.querySelectorAll('ul#primaryNavigation li a')
+    let smallNav = document.querySelector('nav#smallNavArea')
     
     // create new list items for the bottom of the page
     let newList = document.createElement('ul')
@@ -9,8 +10,10 @@ function duplicateMenu() {
         let newLI = document.createElement('li')
         let newLink = document.createElement('a')
         newLink.setAttribute('href', menuItem.getAttribute('href'))
-        // 'copy' the textContent from upper menu to new menu
-        // append children to make them appear in the DOM
+        newLI.innerHTML = `<a href=${newLink}> ${menuItem.innerHTML}</a>`
+        newList.appendChild(newLI)
+    // append children to make them appear in the DOM
+        smallNav.append(newList)
     })
 }
 
